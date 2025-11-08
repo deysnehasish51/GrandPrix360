fe4d74d97bb46bc1e425c697b0eb9ff6d108776f
Snehasish Dey <deysnehasish51@gmail.com>
2025-11-08 10:36:35 +0530

    Club Page is Added

---
+++ js/club.js
@@ -0,0 +1,28 @@
+const slides = document.querySelectorAll('.hero-slide');
+const nextBtn = document.querySelector('.right-arrow');
+const prevBtn = document.querySelector('.left-arrow');
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach((slide, idx) => {
+    slide.classList.remove('active');
+
+    void slide.offsetWidth;
+    if (idx === i) slide.classList.add('active');
+  });
+}
+
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+});
+
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+});
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 5000);
